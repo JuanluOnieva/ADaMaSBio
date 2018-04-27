b25284d61e138607931a1afb6fca164fb8b1f765
mmp1100000 <marcomunozperez@uma.es>
2018-04-27 13:00:30 +0200

    Final commit

--- src/main/java/org/uma/BDB2018/Driver/App.java
+++ src/main/java/org/uma/BDB2018/Driver/App.java
@@ -8,6 +8,7 @@ public class App {
     public static void main( String[] args ) throws SQLException {
         //create connection for a server installed in localhost, with a user "root" with no password
     	GenericSqlConnection con = new PostgreSqlConnection();
+    	con.connect("chebi_basic");
     	con.executeQuery("SELECT compound_id FROM names where name like 'water'");
     	System.out.println(con.getRows());
     	System.out.println("Execution time: " + con.getTime() + "ms");
