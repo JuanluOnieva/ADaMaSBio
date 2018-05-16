274e1d3289d3c81314a3c96705b4e2c8e8e1ad7f
Unknown <marcomunozperez@uma.es>
2018-05-16 23:20:12 +0200

    Improved implementation and added own class
    Now xml class is abstract. 
    chemicalData class not fully implemented yet.

--- src/main/java/org/uma/BDB2018/xml/xml.java
+++ src/main/java/org/uma/BDB2018/xml/xml.java
@@ -5,74 +5,49 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 
 import org.uma.BDB2018.Interfaz.archivo;
 
-public class xml {
+import javax.xml.stream.events.Attribute;
 
-	private ResultSet result;
-	private String idprev;
-	private FileWriter pw;
-	private String id;
+public abstract class xml {
+
+	ResultSet result;
+	String idprev;
+	FileWriter pw;
+	String id;
 	
 	public xml (ResultSet result) throws SQLException, IOException {
 		this.result = result;
 		idprev = "nada";
     	File fichero = new File("prueba.txt");
        	pw = new FileWriter(fichero, true);
-		createXML();
 	}
 	
-	public void createXML() throws SQLException, IOException {
-		int cnt = 2;
-		String label = null;
-		String key = null;
-		int num = result.getMetaData().getColumnCount();
-		pw.write(startLabel("table") + "\n");
-		while (result.next()) {
-			id = result.getString("id");
-			if(!result.wasNull() && !idprev.equals("nada") && !idprev.equals(id))
-				pw.write(endLabel("id") + "\n");
-			if(!result.wasNull() && !id.equals(idprev))
-				pw.write(startLabel("id") + id + "\n");
-			List<String> rowsXML = new LinkedList<>();
-			while (cnt <= num) {
-				label = result.getMetaData().getColumnLabel(cnt);
-				key = result.getString(cnt);
-				if(result.wasNull())
-					key = "NULL";
-				else if(id.equals(idprev)) {
-					key = result.getString("secondary_id");
-					rowsXML.add(startLabel("secondary_id") + key + endLabel("secondary_id"));
-					break;
-				}
-				rowsXML.add(startLabel(label) + key + endLabel(label));
-				cnt++;
-			}
-			System.out.println(rowsXML.toString());
-			escribir(rowsXML);
-			rowsXML.clear();
-			cnt = 2;
-			idprev = result.getString("id");
+public abstract void createXML() throws SQLException, IOException;
+	
+	String startLabel(String id) {
+		return "<" + id + ">\n";
+	}
+
+	String startLabel(String id, HashMap<String,String> Attributes) {
+		//Creates a label with attributes
+		StringBuilder tag = new StringBuilder("<" + id);
+		for (String key : Attributes.keySet()){
+			tag.append(' ').append(key).append("=\"").append(Attributes.get(key)).append('\"');
 		}
-		pw.write(endLabel("id") + "\n");
-		pw.write(endLabel("table") + "\n");
-		
-		if (null != pw)
-			pw.close();
+		tag.append(">\n");
+		return tag.toString();
+	}
+
+	String endLabel(String id) {
+		return "\n</" + id + ">\n";
 	}
 	
-	public String startLabel(String id) {
-		return "<" + id + ">";
-	}
-	
-	public String endLabel(String id) {
-		return "</" + id + ">";
-	}
-	
-	public void escribir(List<String> list) throws IOException {
+	void escribir(List<String> list) throws IOException {
 		for(String l : list)
 			pw.write(l+"\n");
 		
